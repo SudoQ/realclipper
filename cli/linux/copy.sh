0369269032067cbe7c21f58756d0b69bdb20e059
Simon Johansson <simj91@gmail.com>
2014-08-03 14:36:31 +0200

    Added support for enviromental variables in linux scripts

--- cli/linux/copy.sh
+++ cli/linux/copy.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+API_HOST="localhost"
+if [ "$REALCLIPPER_API_HOST" != "" ]; then
+	API_HOST=$REALCLIPPER_API_HOST
+fi
+
+API_PORT="8080"
+if [ "$REALCLIPPER_API_PORT" != "" ]; then
+	API_PORT=$REALCLIPPER_API_PORT
+fi
+	
 #GET API request storing the response in the clipboard"
-curl -s --request GET localhost:8080/realclipper/api/v0.1/clipboard | xclip -selection clipboard
+curl -s --request GET $API_HOST:$API_PORT/realclipper/api/v0.1/clipboard | xclip -selection clipboard
