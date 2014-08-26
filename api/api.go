40759e841ba67d41b40e7d930a7ea0713a66fbe4
Simon Johansson <simj91@gmail.com>
2014-08-26 19:53:25 +0200

    API updated with martini framework

--- api/api.go
+++ api/api.go
@@ -12,34 +12,23 @@ import (
 
 var clipboard string
 
-/*
-func handler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-		case "GET":
-			log.Printf("Copied %s\n", clipboard)
-			fmt.Fprintf(w, clipboard)
-		case "POST":
-			buf:= new(bytes.Buffer)
-			buf.ReadFrom(r.Body)
-			s := buf.String()
-			clipboard = s
-			log.Printf("Pasted %s\n", clipboard)
-			fmt.Fprintf(w, "ok")
-		default:
-			log.Println("Error")
-	}
-}
-*/
-
 func ClipboardGroup(r martini.Router) {
-	r.Get("/", CopyClipboard)
-	r.Post("/", PasteClipboard)
+	r.Get("", CopyClipboard)
+	r.Post("", PasteClipboard)
 }
 
 func CopyClipboard()(int, string) {
+	log.Printf("Copied %s\n", clipboard)
+	return 200, clipboard
 }
 
-func PasteClipboard()(int, string) {
+func PasteClipboard(w http.ResponseWriter, r *http.Request) {
+	buf:= new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+	s := buf.String()
+	clipboard = s
+	log.Printf("Pasted %s\n", clipboard)
+	fmt.Fprintf(w, "ok")
 }
 
 func main(){
