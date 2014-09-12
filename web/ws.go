f1fe7489cbb50d6190bebd482209f4ef111ed002
Simon Johansson <simj91@gmail.com>
2014-09-12 16:50:00 +0200

    First version of the web service, intended to talk to the API

---
+++ web/ws.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/go-martini/martini"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	m := martini.Classic()
+	m.Use(martini.Static("assets"))
+	env_port := os.Getenv("REALCLIPPER_WEB_PORT")
+	if env_port == "" {
+		env_port = "8080"
+	}
+	port := fmt.Sprintf(":%s", env_port)
+	log.Fatal(http.ListenAndServe(port, m))
+}
